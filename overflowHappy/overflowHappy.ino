aa745fa7671d26f8181de97e3b345b6cdc69d6e2
Luke Mizuhashi <luke.mizuhashi@gmail.com>
2017-03-26 03:07:21 -0700

    Initial commit

---
+++ overflowHappy/overflowHappy.ino
@@ -0,0 +1,71 @@
+const short LDR_PIN = A0;
+const unsigned short SERIAL_BAUD_RATE = 9600;
+const int VOLTAGE_IN_VOLTS = 5;
+
+const unsigned long MAX_TIME_IN_MILLI = 4200000;
+const unsigned short CLOCK_TOLERANCE_IN_MILLI = 500;
+
+const unsigned long MAX_LONG = 4294967295;
+const unsigned short MAX_ANALOG_READ_VALUE = 1023;
+const unsigned short MAX_ANALOG_READ_RATE_IN_MICRO = 100;
+
+const unsigned short MIN_SAMPLE_COUNT = 10;
+unsigned long sampleCount;
+short averageVoltage;
+double standardDeviation;
+
+void initializeData() {
+  sampleCount = 0;
+  averageVoltage = 0;
+  standardDeviation = 0;
+}
+
+void setup() {
+  Serial.begin(SERIAL_BAUD_RATE);
+  initializeData();
+}
+
+void loop() {
+  unsigned long loopStartTime = micros();
+  // checkForClockOverRun();
+  
+  short CURRENT_VOLTAGE = getCurrentVoltage(LDR_PIN,VOLTAGE_IN_VOLTS);
+  sampleCount++;
+  averageVoltage = (averageVoltage + CURRENT_VOLTAGE) / 2;
+
+  // if (sampleCount >= MIN_SAMPLE_COUNT) {
+    // abs(CURRENT_VOLTAGE - averageVoltage);
+    // standardDeviation = sqrt(pow(CURRENT_VOLTAGE - averageVoltage,2)/(sampleCount - 1));
+  // }
+  
+  // String message = "Average: " + averageVoltage;
+  // message += "   Voltage Now: " + CURRENT_VOLTAGE;
+  // message += "   Standard Deviation: ";
+  // message += standardDeviation;
+  Serial.println(CURRENT_VOLTAGE);
+  
+  unsigned long loopEndTime = micros();
+  delayIfRequired(loopStartTime,loopEndTime);
+}
+
+short getCurrentVoltage(short pin,short powerSupplyVoltage) {
+  unsigned short sensorValue = analogRead(pin);
+  float voltage = sensorValue * ( powerSupplyVoltage / MAX_ANALOG_READ_VALUE );
+  return voltage;
+}
+
+void delayIfRequired(unsigned long loopStartTime,unsigned long loopEndTime) {
+  if ( loopEndTime - loopStartTime < MAX_ANALOG_READ_RATE_IN_MICRO ) {
+    delay(MAX_ANALOG_READ_RATE_IN_MICRO - loopEndTime - loopStartTime);
+  }
+}
+
+void checkForClockOverRun() {
+  if ( MAX_TIME_IN_MILLI - millis() <= CLOCK_TOLERANCE_IN_MILLI ) {
+    initializeData();
+    while (millis() > MAX_TIME_IN_MILLI / 2) {
+      // Do nothing.
+    }
+  }
+}
+
---
+++ sketch_mar25a/sketch_mar25a.ino
@@ -0,0 +1,93 @@
+/*
+
+*/
+
+const int SENSOR_IN = A0;
+const short MAX_ANALOG_READ_MICRO = 100; // One read per 100 microseconds
+boolean beamBroken;
+
+const int BLUE_OUT = 2;
+const int GREEN_OUT = 3;
+const int RED_OUT = 4;
+
+double minVoltage;
+double maxVoltage;
+const double MAX_DOUBLE_TRIGGER_VOLTS = 0.02;
+
+void setup() {
+    Serial.begin(9600);
+    pinMode(BLUE_OUT,OUTPUT);
+    pinMode(GREEN_OUT,OUTPUT);
+    pinMode(RED_OUT,OUTPUT);
+}
+
+void loop() {
+    long loopStartMicro = micros();
+    
+    const float VOLTAGE = getVoltage(SENSOR_IN);
+    updateMin(VOLTAGE);
+    updateMax(VOLTAGE);
+    updateState(VOLTAGE);
+    updateView();
+
+    writeToSerial(VOLTAGE);
+    waitForNextLoop(loopStartMicro);
+}
+
+void updateState(double voltage) {
+  double triggerLevel = maxVoltage - MAX_DOUBLE_TRIGGER_VOLTS;
+  beamBroken = (voltage < triggerLevel);
+}
+
+void updateView() {
+  if (beamBroken) {
+    digitalWrite(BLUE_OUT,HIGH);
+  } else {
+    digitalWrite(BLUE_OUT,LOW);
+  }
+}
+
+void writeToSerial(double voltage) {
+  String message = " Voltage Now: ";
+  message += voltage;
+
+  message += " Minimum Voltage: ";
+  message += minVoltage;
+
+  message += " Maximum Voltage: ";
+  message += maxVoltage;
+  
+  Serial.println(message);
+}
+
+void waitForNextLoop(long loopStartMicro) {
+  long loopStopMicro = micros();
+  long timeSpentThisLoop = loopStopMicro - loopStartMicro;
+  if (timeSpentThisLoop < MAX_ANALOG_READ_MICRO) {
+    delay(MAX_ANALOG_READ_MICRO - timeSpentThisLoop * 1000);
+  }
+}
+
+float getVoltage(int pin) {
+    float sensorValue = analogRead(pin);
+    return sensorValue * ( 5.0 / 1023.0);
+}
+
+void updateMin(float voltage) {
+  if (!minVoltage) {
+    minVoltage = voltage;
+  }
+  if (voltage < minVoltage) {
+    minVoltage = voltage;
+  }
+}
+
+void updateMax(float voltage) {
+  if (!maxVoltage) {
+    maxVoltage = voltage;
+  }
+  if (voltage > maxVoltage) {
+    maxVoltage = voltage;
+  }
+}
+
